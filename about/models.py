af2f32b1bf06a0f94fe0a644d419c75a9666dbd2
Kai Lu <kaisiulu@gmail.com>
2020-01-16 21:48:01 -0500

    fixed detail view and image issue

--- about/models.py
+++ about/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 class About(models.Model):
-    description = models.TextField()
-    image = models.FilePathField(path='/img')
+  description = models.TextField()
+  image = models.FilePathField(
+      path='/Users/kailu/Desktop/Sides/personal_website/about/static/img')
--- projects/models.py
+++ projects/models.py
@@ -5,4 +5,5 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     technologies = models.CharField(max_length=200)
-    image = models.FilePathField(path='/img')
+    image = models.FilePathField(
+        path='/Users/kailu/Desktop/Sides/personal_website/projects/static/img')
