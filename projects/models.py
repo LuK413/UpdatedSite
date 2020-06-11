5e1a303330b4835812196004c104c5bc55b161b0
Kai Lu <kaisiulu@gmail.com>
2020-06-11 01:34:25 -0400

    touched up on the projects app

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
+    previewDescription = models.CharField(max_length=300, default='preview')
     description = models.TextField()
     technologies = models.CharField(max_length=200)
     image = models.ImageField()
+    link = models.URLField(default='http://127.0.0.1:8000/')
