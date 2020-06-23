144a6246fa94c31842a6ef25f24481513d64e6ea
Kai Lu <kaisiulu@gmail.com>
2020-06-22 22:43:22 -0400

    WIP parallax and colour theme

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'personal_website/static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
